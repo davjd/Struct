246433a2413029eb9a57ee119b0be72ac72f3f71
youngthvg <davidbepic@gmail.com>
2016-12-07 01:10:07 -0500

    Might need to change data structure...

--- Queue.cpp
+++ Queue.cpp
@@ -15,7 +15,7 @@
  *
  * =====================================================================================
  */
-#include <stdlib.h>
+#include <iostream>
 using namespace std;
 
 class Queue{
@@ -23,10 +23,41 @@ class Queue{
 	private:
 		int* values;
 		int size;
+		int tail;
 	public:
 		Queue(int size){
-			this.size = size;
+			this->size = size;
+			this->values = new int[size];
+			this->tail = -1;
 		}
+		bool isEmpty();
+		bool isFull();
+		void enqueue(int itm);
+		int getSize();
+		int dequeue();
+		void resize(int newSize);
+};
+Queue::isEmpty(){
+	return (size == 0);
+}
+Queue::isFull(){
+	// check if the tail index is greater, if so return 
+	return (this->tail >= (this->size - 1));
+}
+Queue::enqueue(int itm){
+	++this->tail; // update size.
+	//check if there is enough space:
+	if(isFull()){
+		// if its full, use table-doubling.
+		resize((this->size * 2));
+	}
+	values[this->tail] = itm; // add itm.
+}
+Queue::getSize(){
+	return this->size;
+}
+Queue::dequeue(){
+	// might need to change data structure ..
 }
 
 int main(){
