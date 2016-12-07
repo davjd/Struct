d62b8f0496969cec47772005ac67ca60630511f0
youngthvg <davidbepic@gmail.com>
2016-12-06 22:13:07 -0500

    Added Queue class.

---
+++ Queue.cpp
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Queue.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/05/2016 11:35:19 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  youngthvg, 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+using namespace std;
+
+class Queue{
+	// interface for Queue.
+	private:
+		int* values;
+		int size;
+	public:
+		Queue(int size){
+			this.size = size;
+		}
+}
+
+int main(){
+
+	return 0;
+}
