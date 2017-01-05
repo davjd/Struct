3a13c6e26edefa753bd9061c1016ceb8f3b5ee9c
youngthvg <davidbepic@gmail.com>
2017-01-04 22:29:57 -0500

    Finished Node implementation

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -10,7 +10,7 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
+ *         Author:  youngthvg, 
  *   Organization:  
  *
  * =====================================================================================
@@ -19,16 +19,15 @@
 #include <iostream>
 int main(){
 	std::cout << "---+COMPILED+--\n";
-	Node a;
-	Node* b;
-	Node* c;
-	//a->setCurrent(9);
-	//c->setCurrent(11);
-	//b->setCurrent(10);
-	//b->setRight(c);
-	//b->setLeft(a);
-	std::cout << "----COMPILED---\n" << a.getCurrent() << "\n";
-	//std::cout << "current: " << b->getCurrent() << "\nleft: " << b->getLeft()->getCurrent() << "\nright: " << b->getRight()->getCurrent() << "\n";
+	Node* a = new Node();
+	Node* b = new Node();
+	Node* c = new Node();
+	a->setCurrent(9);
+	c->setCurrent(11);
+	b->setCurrent(10);
+	b->setRight(c);
+	b->setLeft(a);
+	std::cout << "current: " << b->getCurrent() << "\nleft: " << b->getLeft()->getCurrent() << "\nright: " << b->getRight()->getCurrent() << "\n";
 
 	return 0;
 }
